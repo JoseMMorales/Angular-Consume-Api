9463fd00873a9a3f28d929fe4732bd9f02df4447
JoseMMorales <josemulamorales33@hotmail.com>
2021-10-31 09:59:46 +0100

    Adding rouing modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { EditComponent } from './views/edit/edit.component';
+import { LoginComponent } from './views/login/login.component';
+import { NewComponent } from './views/new/new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'Login', pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'edit', component:EditComponent},
+  {path:'new', component:NewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  LoginComponent,
+  DashboardComponent,
+  EditComponent,
+  NewComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
-import { LoginComponent } from './views/login/login.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { NewComponent } from './views/new/new.component';
-import { EditComponent } from './views/edit/edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    DashboardComponent,
-    NewComponent,
-    EditComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
