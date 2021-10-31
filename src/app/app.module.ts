ca87f361511239c3b11862706312d50206e46033
JoseMMorales <josemulamorales33@hotmail.com>
2021-10-31 11:04:07 +0100

    Connecting login form with ts file to get parameters user and password using formGroup and ngSubmit in onLogin method. Also importing ReactiveFormsModule, FormsModule in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './templates/header/header.component';
 import { FooterComponent } from './templates/footer/footer.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FooterComponent } from './templates/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    user : new FormControl('', Validators.required),
+    password : new FormControl('', Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onLogin(form: object) {
+    console.log(form);
+    
   }
 
 }
