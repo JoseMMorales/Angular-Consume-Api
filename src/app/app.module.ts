385fe78df1930545ae2b6568d03cfbaaadbeacf9
JoseMMorales <josemulamorales33@hotmail.com>
2021-10-31 09:46:48 +0100

    Create Consume API project and components login, footer, header dashboard edit new, service of the API and interfaces folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { NewComponent } from './views/new/new.component';
+import { EditComponent } from './views/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
